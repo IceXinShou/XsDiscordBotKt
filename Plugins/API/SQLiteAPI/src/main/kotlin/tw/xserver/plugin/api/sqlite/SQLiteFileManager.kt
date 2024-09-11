318677ed675458470ac146cc7015dcbfe07e3369
XinShou <icexinshou@gmail.com>
2024-09-11 14:41:20 +0800

    Fix Bugs Rename some methods

--- Plugins/API/SQLiteAPI/src/main/kotlin/tw/xserver/plugin/api/sqlite/SQLiteFileManager.kt
+++ Plugins/API/SQLiteAPI/src/main/kotlin/tw/xserver/plugin/api/sqlite/SQLiteFileManager.kt
@@ -5,37 +5,48 @@ import java.sql.Connection
 import java.sql.DriverManager
 import javax.management.openmbean.KeyAlreadyExistsException
 
-class SQLiteFileManager {
-    private val dbConn: MutableMap<Long, Connection> = HashMap()
+object SQLiteFileManager {
+    private val dbConn: MutableMap<String, PathConnection> = HashMap()
 
-    fun getConnection(uniqueKey: Long): Connection = dbConn[uniqueKey]!!
+    init {
+        Class.forName("org.sqlite.JDBC")
+    }
 
-    fun disconnect(uniqueKey: Long) {
+    fun getConnection(uniqueKey: String): Connection? = dbConn[uniqueKey]?.connection
+
+    @Synchronized
+    fun addConnection(uniqueKey: String, file: File): Connection {
+        if (uniqueKey in dbConn) {
+            throw KeyAlreadyExistsException("Connection key $uniqueKey already exists.")
+        }
+
+        val connection = DriverManager.getConnection("jdbc:sqlite:${file.canonicalPath}")
+        dbConn[uniqueKey] = PathConnection(file.canonicalPath, connection)
+
+        return connection
+    }
+
+    @Synchronized
+    fun removeConnection(uniqueKey: String) = disconnect(uniqueKey)
+
+    @Synchronized
+    fun disconnect(uniqueKey: String) {
         dbConn[uniqueKey]?.let {
-            it.close()
+            it.connection.close()
             dbConn.remove(uniqueKey)
         } ?: throw NoSuchElementException("No connection associated with key: $uniqueKey")
     }
 
-    fun addFileConnection(uniqueKey: Long, file: File): Connection {
-        return addFileConnection(uniqueKey, file, false)
-    }
-
-    fun getOrDefaultConnection(uniqueKey: Long, file: File): Connection {
-        if (!dbConn.containsKey(uniqueKey))
-            return addFileConnection(uniqueKey, file, true)
-
-        return getConnection(uniqueKey)
-    }
-
     @Synchronized
-    private fun addFileConnection(uniqueKey: Long, file: File, skipCheck: Boolean): Connection {
-        if (!skipCheck && dbConn.containsKey(uniqueKey))
-            throw KeyAlreadyExistsException("Connection key $uniqueKey already exists.")
-
-        val connection = DriverManager.getConnection("jdbc:sqlite:${file.canonicalPath}")
-        dbConn[uniqueKey] = connection
-
-        return connection
+    fun deleteDatabase(uniqueKey: String) {
+        dbConn[uniqueKey]?.filePath?.let { path ->
+            disconnect(uniqueKey)
+            File(path).delete()
+        }
     }
+
+    private data class PathConnection(
+        val filePath: String,
+        val connection: Connection,
+    )
 }
