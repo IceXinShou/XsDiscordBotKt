f5aa3cbb5c8ca9927916d9d43735c4eb2f358bc4
XinShou <icexinshou@gmail.com>
2024-11-08 00:13:03 +0800

    refactor: remove unused variable

--- Plugins/BotInfo/src/main/kotlin/tw/xserver/plugin/botinfo/Event.kt
+++ Plugins/BotInfo/src/main/kotlin/tw/xserver/plugin/botinfo/Event.kt
@@ -3,8 +3,6 @@ package tw.xserver.plugin.botinfo
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.DiscordLocale
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import tw.xserver.loader.localizations.LangManager
 import tw.xserver.loader.plugin.PluginEvent
 import tw.xserver.loader.util.FileGetter
@@ -16,7 +14,6 @@ import java.io.File
 
 object Event : PluginEvent(true) {
     internal val PLUGIN_DIR_FILE = File("./plugins/BotInfo/")
-    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
 
     override fun load() {
         reloadAll()
