11855a4f63f7e13d47b0cd7404c584bc5b82c990
XinShou <icexinshou@gmail.com>
2024-11-15 04:08:43 +0800

    fix: fix member cache bug

--- Plugins/TicketAddons/src/main/kotlin/tw/xserver/plugin/addons/ticket/Event.kt
+++ Plugins/TicketAddons/src/main/kotlin/tw/xserver/plugin/addons/ticket/Event.kt
@@ -2,12 +2,9 @@ package tw.xserver.plugin.addons.ticket
 
 import com.charleskorn.kaml.Yaml
 import com.charleskorn.kaml.decodeFromStream
-import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent
-import net.dv8tion.jda.api.events.session.ReadyEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import tw.xserver.loader.base.MainLoader.jdaBot
 import tw.xserver.loader.plugin.PluginEvent
 import tw.xserver.loader.util.FileGetter
 import tw.xserver.plugin.addons.ticket.serializer.MainConfigSerializer
@@ -18,7 +15,6 @@ object Event : PluginEvent(true) {
     internal val PLUGIN_DIR_FILE = File("./plugins/TicketAddons/")
     private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
     internal lateinit var config: MainConfigSerializer
-    internal lateinit var targetMember: Member
         private set
 
     override fun load() {
@@ -43,12 +39,6 @@ object Event : PluginEvent(true) {
 
     override fun reloadLang() {
         fileGetter.exportDefaultDirectory("./lang")
-    }
-
-    override fun onReady(event: ReadyEvent) {
-        targetMember = jdaBot
-            .getGuildById(config.guildId)!!
-            .retrieveMemberById(config.userId).complete()
     }
 
     override fun onChannelCreate(event: ChannelCreateEvent) {
--- Plugins/TicketAddons/src/main/kotlin/tw/xserver/plugin/addons/ticket/TicketAddons.kt
+++ Plugins/TicketAddons/src/main/kotlin/tw/xserver/plugin/addons/ticket/TicketAddons.kt
@@ -3,9 +3,9 @@ package tw.xserver.plugin.addons.ticket
 import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent
 import net.dv8tion.jda.api.interactions.DiscordLocale
+import tw.xserver.loader.base.MainLoader.jdaBot
 import tw.xserver.plugin.addons.ticket.Event.PLUGIN_DIR_FILE
 import tw.xserver.plugin.addons.ticket.Event.config
-import tw.xserver.plugin.addons.ticket.Event.targetMember
 import tw.xserver.plugin.creator.message.MessageCreator
 import java.io.File
 import java.util.concurrent.TimeUnit
@@ -16,7 +16,11 @@ object TicketAddons {
     fun onCreate(event: ChannelCreateEvent) {
         if (!event.isFromGuild && event.guild.id != config.guildId) return
         if (!event.channel.name.startsWith(config.prefix)) return
-        if (targetMember.onlineStatus != OnlineStatus.OFFLINE) return
+        if (jdaBot.getGuildById(config.guildId)!!
+                .retrieveMemberById(config.userId).complete()
+                .onlineStatus != OnlineStatus.OFFLINE
+        ) return
+
         val delay: Long = config.delayMillis
 
         event.channel.asTextChannel().sendMessage(
